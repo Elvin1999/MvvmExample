216144c93bc98bc489a182d81e0439f952b71e23
e.camalzade <e.camalzade@STHQ011A-01>
2021-09-07 10:19:10 +0400

    Add project files.

---
+++ WpfApp1/Repository/FakeRepo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+
+namespace WpfApp1.Repository
+{
+   public class FakeRepo
+    {
+        public List<Printer> GetAll()
+        {
+            return new List<Printer>
+            {
+                new Printer
+                {
+                    Color="Red",
+                    Model="R100",
+                    Vendor="Canon"
+                },
+                new Printer
+                {
+                    Color="Blue",
+                    Model="LLL100",
+                    Vendor="Hp"
+                },
+                new Printer
+                {
+                    Color="SkyBlue",
+                    Model="R100",
+                    Vendor="M100"
+                },
+                new Printer
+                {
+                    Color="Indigo",
+                    Model="C202",
+                    Vendor="Varta"
+                }
+            };
+        }
+    }
+}
---
+++ WpfApp1/ViewModels/AppViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+using WpfApp1.Repository;
+
+namespace WpfApp1.ViewModels
+{
+   public class AppViewModel:BaseViewModel
+    {
+        public FakeRepo PrinterRepository { get; set; }
+        public ObservableCollection<Printer> Printers { get; set; }
+
+        public AppViewModel()
+        {
+            PrinterRepository = new FakeRepo();
+            Printers = new ObservableCollection<Printer>(PrinterRepository.GetAll());
+        }
+    }
+}
